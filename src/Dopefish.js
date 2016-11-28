09ea31edce95b0bf6fc704fdc2a1380be56752db
Peter Breen <pbreen@gmail.com>
2016-11-28 10:24:34 -0800

    fixes image location from prior commit, imports images directly into js

--- src/Dopefish.js
+++ src/Dopefish.js
@@ -1,13 +1,16 @@
 /*
-this is the parent component for any instances of a Dopefish, such that App.js could just call <Dopefish for multiple fish at once. Specifically, it contains state for a fish: what coordinates (which determines location on page) and what direction it is 'swimming' (which determines image to display) as well as a click handler for the onClick burp interact.
+this is the parent component for any instances of a Dopefish, such that App.js could just call it repeatedly for multiple fish at once. Specifically, it contains state for a fish: what coordinates (which determines location on page) and what direction it is 'swimming' (which determines image to display) as well as a click handler for the onClick burp interact.
 */
 
 import React, { Component } from 'react';
+import imgLeft from './img/swimfish.gif';
+import imgRight from './img/swimfish2.gif';
+import imgBurp from './img/burpfish.gif';
 
 class Dopefish extends Component {
   render() {
     return (
-      <img src="./img/swimfish.gif" alt="A swimming dopefish"/>
+      <img src={imgLeft} alt="A swimming dopefish"/>
     );
   }
 }
