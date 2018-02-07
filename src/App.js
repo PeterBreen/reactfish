4202a674daa1ba54616e17c7bf071e7d8ca94da5
Peter Breen <pbreen@gmail.com>
2018-02-07 10:10:14 -0800

    start of new approach to conditionally rendering number of fishes, beautified code because yikes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Dopefish from './Dopefish.js';
 
 class App extends Component {
-  // get viewport width/height dimensions, will handle resizing, from http://stackoverflow.com/questions/36862334/get-viewport-window-height-in-reactjs
   constructor(props) {
     super(props);
-    this.state = { width: null, height: null };
+    this.state = {
+      width: null,
+      height: null,
+      numFish: 6
+    };
     this.updateFishtankDimensions = this.updateFishtankDimensions.bind(this);
-    //
   }
 
+  // get viewport width/height dimensions, will handle resizing, from http://stackoverflow.com/questions/36862334/get-viewport-window-height-in-reactjs
   componentDidMount() {
     this.updateFishtankDimensions();
     window.addEventListener('resize', this.updateFishtankDimensions);
@@ -20,28 +23,26 @@ class App extends Component {
   }
 
   updateFishtankDimensions() {
-      this.setState({ width: this.refs.fishtank.clientWidth, height: this.refs.fishtank.clientHeight });
+    this.setState({width: this.refs.fishtank.clientWidth, height: this.refs.fishtank.clientHeight});
   }
-
 
   render() {
 
     let fishQty = null;
     if (this.state.width > 640) {
-        fishQty = <div><Dopefish width={this.state.width} height={this.state.height} /><Dopefish width={this.state.width} height={this.state.height} /><Dopefish width={this.state.width} height={this.state.height} /><Dopefish width={this.state.width} height={this.state.height} /> </div>
-      } else {
-        fishQty = <div><Dopefish width={this.state.width} height={this.state.height} /><Dopefish width={this.state.width} height={this.state.height} /><Dopefish width={this.state.width} height={this.state.height} /></div>
-      }
-
-    return (
-      <div className="fishtank" ref="fishtank">
-{ this.state.width && this.state.height &&
-        <div className="fishes">
-          {fishQty}
-        </div>
-}
+      fishQty = <div><Dopefish width={this.state.width} height={this.state.height}/><Dopefish width={this.state.width} height={this.state.height}/><Dopefish width={this.state.width} height={this.state.height}/><Dopefish width={this.state.width} height={this.state.height}/>
       </div>
-    );
+    } else {
+      fishQty = <div><Dopefish width={this.state.width} height={this.state.height}/><Dopefish width={this.state.width} height={this.state.height}/><Dopefish width={this.state.width} height={this.state.height}/></div>
+    }
+
+    return (<div className="fishtank" ref="fishtank">
+      {
+        this.state.width && this.state.height && <div className="fishes">
+            {fishQty}
+          </div>
+      }
+    </div>);
   }
 }
 
